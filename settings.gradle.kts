75145b6093079e370efc070efbb82841e83e1708
GitHub Action <action@github.com>
2025-09-02 09:50:53 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
+
+rootProject.name = "eran-test-2025-09-02"
